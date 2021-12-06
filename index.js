c160c0a7bb49212d46bfe4aa23a6564cf58f3202
Frederick <admin@freddyscovell.com>
2021-12-06 15:48:03 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+/*function updateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value; 
+    return employee;
+    }
+
+    const employee ={name: 'temp', streetAdress: 'temp'};
+   */
+
+const employee ={name: 'temp', streetAdress: 'temp'};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...obj};
+    newEmployee[key] = value; 
+    return newEmployee;
+}
+ 
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
